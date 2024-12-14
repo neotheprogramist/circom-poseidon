d60f915d0e6412acc70193a8ac5eab665f723964
Neo <neotheprogramist@proton.me>
2024-12-14 17:51:02 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+To run circom:
+`./run-circom.sh`
+output is logged and also it is in `target/public.json`
+
+To run python:
+`python -m venv .venv`
+`source .venv/bin/activate`
+`pip install -r poseidon-hash/requirements.txt`
+`python poseidon-hash/main.py`
---
+++ run-circom.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+circom circuits/example.circom --r1cs --wasm --sym --c --output target
+node target/example_js/generate_witness.js target/example_js/example.wasm circuits/example.input.json target/example.wtns
+npx snarkjs groth16 setup target/example.r1cs target/pot12_final.ptau target/example_0000.zkey
+npx snarkjs groth16 prove target/example_0000.zkey target/example.wtns target/proof.json target/public.json
